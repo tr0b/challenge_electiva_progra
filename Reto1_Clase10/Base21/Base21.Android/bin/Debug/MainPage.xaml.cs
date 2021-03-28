dd7a2ee75d35680260eb49a6c98713103cad06c3
tr0b <trobin3000600@gmail.com>
2021-03-28 15:29:17 -0600

    Challenge progra - Ejercicio Completo

---
+++ Reto1_Clase10/Base21/Base21.Android/bin/Debug/MainPage.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using Xamarin.Forms;
+using System.Text.RegularExpressions;
+using D = System.Double;
+
+namespace Base21
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+
+    public partial class MainPage : ContentPage
+    {
+        static string nombreArchivo = "prueba.txt";
+        static string ruta = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+        static string rutaCompleta = Path.Combine(ruta, nombreArchivo);
+        public MainPage()
+        {
+            InitializeComponent();
+            BtnSuma.Clicked += ejecutarOperacion;
+            BtnRest.Clicked += ejecutarOperacion; 
+            BtnDiv.Clicked += ejecutarOperacion;
+            BtnMult.Clicked += ejecutarOperacion;
+            BtnLimp.Clicked += BtnLimp_Clicked;
+            tbSave.Clicked += guardar;
+            tbResultados.Clicked += onResultadosClicked;
+        }
+
+        private void onResultadosClicked(object sender, EventArgs e)
+        {
+            ((NavigationPage)this.Parent).PushAsync(new Resultados());
+        }
+
+        /*Guarda operacion y resultado en archivo de texto*/
+        private void guardar(object sender, EventArgs e)
+        {
+            string resultado = lblres.Text;
+            using (StreamWriter sw = File.AppendText(rutaCompleta)){ sw.WriteLine(resultado); }
+        }
+
+        /*Ejecuta operacion. Posibles operaciones son: suma, resta, multiplicacion, o division*/
+        private void ejecutarOperacion(object sender, EventArgs e)
+        {
+            double primer_numero, segundo_numero;
+            if (!(D.TryParse(num1.Text, out primer_numero) && D.TryParse(num2.Text, out segundo_numero))) return;
+            string resultado = "";
+            var boton_operacion = (Button)sender;
+            string operacion = boton_operacion.ClassId;
+            switch (operacion)
+            {
+                case "+":
+                    resultado = (primer_numero + segundo_numero).ToString();
+                    break;
+                case "-":
+                    resultado = (primer_numero - segundo_numero).ToString();
+                    break;
+                case "*":
+                    resultado = (primer_numero * segundo_numero).ToString();
+                    break;
+                case "/":
+                    if (segundo_numero == 0) { resultado = "?"; break; }
+                    resultado = (primer_numero / segundo_numero).ToString();
+                    break;
+            }
+            lblres.Text = $"{primer_numero} {operacion} {segundo_numero} = {resultado}";
+        }
+        private void BtnLimp_Clicked(object sender, EventArgs e)
+        {
+            num1.Text = String.Empty;
+            num2.Text = String.Empty;
+            res.Text = String.Empty; 
+        }
+    }
+
+}
---
+++ Reto1_Clase10/Base21/Base21/Resultados.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Base21
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Resultados : ContentPage
+    {
+        static string nombreArchivo = "prueba.txt";
+        static string ruta = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+        static string rutaCompleta = Path.Combine(ruta, nombreArchivo);
+        public Resultados()
+        {
+            InitializeComponent();
+            fetchResultados();
+            tbHome.Clicked += onHomeClicked;
+        }
+
+        private void onHomeClicked(object sender, EventArgs e)
+        {
+            ((NavigationPage)this.Parent).PushAsync(new MainPage());
+        }
+
+        private void fetchResultados()
+        {
+            if (File.Exists(rutaCompleta)) { 
+                using (var lector = new StreamReader(rutaCompleta, true)) {
+                    string TextoLeido;
+                    while ((TextoLeido = lector.ReadLine()) != null) {
+                        resultados.Text += $"{TextoLeido}\n"; 
+                    } 
+                } 
+            }
+        }
+    }
+}
